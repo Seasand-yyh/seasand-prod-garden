4dc3fdcc734f7eed493d7df96190f35ab70dc9e8
yangyh115 <yangyh115@meicloud.com>
2022-03-02 10:07:27 +0800

    【proj】 Gridster Samples

---
+++ content/projects/gridster/demo/js/common.js
@@ -0,0 +1,30 @@
+/**
+ * common.js
+ * @author Seasand-yyh
+ * @date 2019-03-26
+ */
+var common = {
+
+	uuid: function() {
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+      d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+	},
+
+	showTip: function(opt) {
+
+	},
+
+	showDialog: function(opt) {
+
+	},
+
+
+
+};
